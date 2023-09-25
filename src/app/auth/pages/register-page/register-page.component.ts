c4f359b373468e18f9ba67be471fef3e968f1b44
Álex <alejandro.mcamps@bosonit.com>
2023-09-25 13:31:13 +0200

    Validators service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
 import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
@@ -9,17 +10,18 @@ import * as customValidators from 'src/app/shared/validators/validators';
 export class RegisterPageComponent {
 
 public myForm : FormGroup = this.fb.group({
-  name: ['', [Validators.required]],
-  email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
-  username: ['',[Validators.required, customValidators.cantBeStrider]],
+  name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+  email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+  username: ['',[Validators.required, this.validatorsService.cantBeStrider]],
   password: ['',[Validators.required, Validators.minLength(6)]],
   password2: ['',[Validators.required]],
 });
 
-constructor(private fb : FormBuilder) {}
+constructor(private fb : FormBuilder,
+  private validatorsService: ValidatorsService) {}
 
   isValidField( field: string ) {
-
+   return this.validatorsService.isValidField(this.myForm , field);
   }
 
 
